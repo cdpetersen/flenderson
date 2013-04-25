b457d554668a06165cc7c6d2df1e4db69c0d7c68
Ted Hansen <ted@detroitlabs.com>
2013-04-25 16:20:07 -0400

    default rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flenderson::Application.config.secret_token = '5132a4b24b7c3c43163be8d15508031251e5dd7b5adc4d82af85bc7681e4331d2bbf6b98f76bb6d98bac23195a9fb378b119fc0f2aad5d240ff328fc5b4ed8eb'
